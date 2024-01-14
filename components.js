ac2045fe69c3e2002d72857b2a29704b90401fcd
programistica <evenogueiratecnologia@gmail.com>
2024-01-14 17:37:06 -0300

    initial commit

---
+++ components.js
@@ -0,0 +1,31 @@
+function Menu() {
+  return `
+       
+            <ul >
+                <li><span class="material-symbols-outlined">
+                home
+                </span>Home</li>
+                <li><span class="material-symbols-outlined">
+                folder_open
+                </span>Repositories</li>
+            </ul>
+        
+    `;
+}
+
+function Header() {
+  return `
+        
+        <span class="material-symbols-outlined">
+        menu
+        </span><h1>Programistica</h1>
+       
+    `;
+}
+
+function Home() {
+  return `
+    <img id="avatar" src="https://avatars.githubusercontent.com/u/890851?s=280&v=4" alt="logo" class="logo">
+    <h2>Eve N</h2>
+    `;
+}
